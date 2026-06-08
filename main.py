2a34e8ff87fba7cfb07b65c4d1283ce57b8cfbfd
Arian <arianpanahi6@gmail.com>
2026-06-08 16:30:20 +0330

    rename cid to customer_id and bebug it

--- main.py
+++ main.py
@@ -10,28 +10,27 @@ bot=telebot.TeleBot(API_TOKEN)
 
 #add your message in line 27,31
 
-find_spam_data=dict()     #{cid:[len,time],...}
-def find_spam(cid,maximum_time,maximum_watch):  
+find_spam_data=dict()     #{customer_id:[len,time],...}
+def find_spam(customer_id,maximum_time=1,maximum_watch=5):  
     global find_spam_data
     now = time.time()
-    if cid not in find_spam_data:
-        find_spam_data[cid]=[0,time.time()]
-    if now-find_spam_data[cid][1] < maximum_time:
-        find_spam_data[cid][0] += 1
-        print(find_spam_data[cid][0])
-    if find_spam_data[cid][0]%maximum_watch==0:
-        data=get_customer_black(cid)
+    if customer_id not in find_spam_data:
+        find_spam_data[customer_id]=[0,time.time()]
+    if now-find_spam_data[customer_id][1] < maximum_time:
+        find_spam_data[customer_id][0] += 1
+    if find_spam_data[customer_id][0]%maximum_watch==0:
+        data=get_customer_black(customer_id)
 
-        if cid not in get_black_list_list():
-            add_customer_black_list(cid,time.time())
-            bot.send_message(cid,'level one 1')
+        if customer_id not in get_black_list_list():
+            add_customer_black_list(customer_id,time.time())
+            bot.send_message(customer_id,'level one 1')
         elif data['STAGE']==1 and data['DON']=='yes':
 
-            add_customer_black_list(cid,time.time(),2)
-            bot.send_message(cid,'level 2')
+            add_customer_black_list(customer_id,time.time(),2)
+            bot.send_message(customer_id,'level 2')
         elif data['STAGE']==2 and data['DON']=='yes':
-            add_customer_black_list(cid,time.time(),2)
-    find_spam_data[cid][1]=time.time()
+            add_customer_black_list(customer_id,time.time(),2)
+    find_spam_data[customer_id][1]=time.time()
 
 
 
@@ -41,7 +40,7 @@ def find_spam(cid,maximum_time,maximum_watch):
 def listener(messages):
     for m in messages:
         # print(m)
-        find_spam(m.chat.id,3,5)
+        find_spam(m.chat.id)
         if m.content_type == 'text':
             print(f"{m.chat.first_name} [{str(m.chat.id)}]: {m.text}")
         elif m.content_type == 'photo':
@@ -57,9 +56,9 @@ bot.set_update_listener(listener)
 
 @bot.message_handler(func = lambda message: True)
 def all_message_handler(message):
-    cid=message.chat.id 
-    if check_black_list(cid) ==False:
-        bot.send_message(cid,message.text)
+    customer_id=message.chat.id 
+    if check_black_list(customer_id) ==False:
+        bot.send_message(customer_id,message.text)
     else:
         # it better do not answer
         pass
@@ -68,18 +67,24 @@ def all_message_handler(message):
 
 def check_find_spam_status(warring_1=60*15,warring_2=3600,sleep_time=60):  #warring_1=15 minute,warring_2=one hour,sleep_time=one minute
     while True:
+        regester_time=time.time()
         now = time.time()
-        for cid in get_black_list_list():
-            data=get_customer_black(cid) #get customer data from black list table 
+        for customer_id in get_black_list_list():
+            data=get_customer_black(customer_id) #get customer data from black list table 
 
             if data['STAGE']==1 and data['DON']=='no':
                 if int(now-data['TIME'] ) > warring_1:
-                    came_customer_black_list(cid)
+                    came_customer_black_list(customer_id)
+                    find_spam_data.pop(customer_id , None)
+
 
             elif data['STAGE']==2 and data['DON']=='no':
                 if int(now-data['TIME']) > warring_2:
-                    came_customer_black_list(cid)
-            
+                    came_customer_black_list(customer_id)
+                    find_spam_data.pop(customer_id , None)
+        if regester_time >= 3600*24:
+            find_spam_data.clear()
+            regester_time=time.time()
         time.sleep(sleep_time)
 t2=threading.Thread(target=check_find_spam_status,args=())#one time in a day check for users that their 100 days is coming to end and send message to admin to check if they have registered project or not
 t2.start() #if you like can add arg
